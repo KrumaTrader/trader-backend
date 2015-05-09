c379dcc1ad281356ebba1c15eeacb3060b97f6c3
Arnob L. Alam <arnoblalam@gmail.com>
2015-05-09 14:18:34 -0400

    Remove unneeded libraries from main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import webapp2
-from models import Product
-from google.appengine.ext import ndb
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-    	self.response.write('Hello, World!')
+        self.response.write('Hello, World!')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
